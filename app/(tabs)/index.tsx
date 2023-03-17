fd0367c1c135866089ebb7ebdb7f64c6750307f8
bigcanoet <123031045+bigcanoet@users.noreply.github.com>
2023-03-17 16:42:38 -0400

    auth screens

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,50 +1,50 @@
-import { Pressable, StyleSheet, Text, Alert } from 'react-native';
+import { StyleSheet, Text, Pressable, Alert } from 'react-native';
 import { View } from '../../components/Themed';
-import { Agenda, AgendaEntry } from "react-native-calendars";
-import events from "../../assets/data/events.json"
+import { Agenda, AgendaEntry } from 'react-native-calendars';
+import events from "../../assets/data/events.json";
+import { Link } from 'expo-router';
+
 
 export default function TabOneScreen() {
-
   const renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
     const fontSize = isFirst ? 16 : 14;
     const color = isFirst ? "black" : "#43515c";
 
     return (
-      <Pressable
-        style={[styles.item, { height: reservation.height }]}
-        onPress={() => Alert.alert(reservation.name)}
-      >
-        <Text style={{ fontSize, color }}>{reservation.name}</Text>
+      <Link href="../modal" asChild>
+        <Pressable
+          style={[styles.item, { height: reservation.height }]}
+          >
+          <Text style={{ fontSize, color }}>{reservation.name}</Text>
       </Pressable>
+      </Link>
     );
   };
 
   const renderEmptyDate = () => {
     return (
       <View style={styles.emptyDate}>
-        <Text>This i an empty date!</Text>
+        <Text>This is empty date!</Text>
       </View>
     );
   };
 
   return (
     <View style={styles.container}>
-        <Agenda 
-        items={events} 
-        selected="2022-11-23"
-        renderItem={renderItem} 
-        renderEmptyDate={renderEmptyDate}
-        showOnlySelectedDayItems
-        />
+          <Agenda 
+          items={events}
+          selected="2022-11-24"
+          renderItem={renderItem}
+          renderEmptyDate={renderEmptyDate} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
-  item: {
+  item : {
     backgroundColor: "white",
     flex: 1,
     borderRadius: 5,
@@ -52,10 +52,9 @@ const styles = StyleSheet.create({
     marginRight: 10,
     marginTop: 17,
   },
-
   emptyDate: {
     height: 15,
     flex: 1,
     paddingTop: 30,
-  }
+  },
 });
